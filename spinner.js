bf55cd11dff14f787fb811905606d8bbd647aa55
JessieLyu <jessie.yujie.lyu@gmail.com>
2020-05-11 12:12:58 +1000

    Pick the list version as the final wheel

--- spinner.js
+++ spinner.js
@@ -4,10 +4,16 @@ const mySpinner = () => {
     let deg = Math.floor(Math.random() * (x - y)) + y;
     document.getElementById('box').style.transform = "rotate(" + deg + "deg)";
 
-    let ele = document.getElementById('mainbox');
-    ele.classList.remove('animate');
-    setTimeout(() => {
-        ele.classList.add('animate');
-    }, 5000);
+    // let ele = document.getElementById('mainbox');
+    // ele.classList.remove('animate');
+    // setTimeout(() => {
+    //     ele.classList.add('animate');
+    // }, 5000);
 
+}
+
+const teamMembers=()=>{
+    let file=require("fs");
+    let text=file.readFileSync("./input.txt");
+    text.split("\n");
 }
