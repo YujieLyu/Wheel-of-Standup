471b3b1ceb7bb05c381004caa83422a1ec1023f5
JessieLyu <jessie.yujie.lyu@gmail.com>
2020-05-11 12:16:47 +1000

    Clean up

--- spinner.js
+++ spinner.js
@@ -3,17 +3,4 @@ const mySpinner = () => {
     let y = 9999;
     let deg = Math.floor(Math.random() * (x - y)) + y;
     document.getElementById('box').style.transform = "rotate(" + deg + "deg)";
-
-    // let ele = document.getElementById('mainbox');
-    // ele.classList.remove('animate');
-    // setTimeout(() => {
-    //     ele.classList.add('animate');
-    // }, 5000);
-
-}
-
-const teamMembers=()=>{
-    let file=require("fs");
-    let text=file.readFileSync("./input.txt");
-    text.split("\n");
 }
